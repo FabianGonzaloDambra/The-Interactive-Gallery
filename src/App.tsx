d94c245651cf5eff24cc1eef0fe55269711dab77
Fabián Gonzalo Dambra <fabiandambra@gmail.com>
2025-02-19 18:52:57 -0300

    Expanding gallery

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import { useEffect, useState } from 'react'
 import './App.css'
 
-//Getting the needed information from Unsplash
+// Defining the API key and URL for fetching random images from Unsplash
 const API_KEY = import.meta.env.VITE_UNSPLASH_API_KEY;
 const URL = `https://api.unsplash.com/photos/random?client_id=${API_KEY}&count=5`
 
-//Setting variable types
+// Defining the TypeScript interface for the Unsplash image structure
 interface UnsplashImage {
   id: string;
   user: {
@@ -18,36 +18,62 @@ interface UnsplashImage {
   alt_description: string;
 }
 
-//Main Function
+// Main component that fetches and displays a gallery of random Unsplash images
 const UnsplashGallery = () => {
-  //Defining Initial State for the images
+  // Defining the initial state to store the fetched Unsplash images
   const [images, setImages] = useState<UnsplashImage[]>([]);
+  const [selectedImg, setSelectedImg] = useState<UnsplashImage | null>(null);
 
-  //Requesting fetch
+  // Making a fetch request to the Unsplash API to get random images
   useEffect(() => {
     fetch(URL)
       .then(response => response.json())
       .then(data => {
         setImages(data)
-        console.log(data)
       })
-      .catch(error => console.error("Error when getting images", error));
+      .catch(error => console.error('Error when getting images', error));
   }, []);
 
-  return <>
-    {images.map(image =>
-      <figure key={image.id}>
-        <img
-          src={image.urls.thumb}
-          alt={image.alt_description}
-        />
-        <figcaption>
-          <h2>{image.alt_description}</h2>
-          <p>{image.user.name}</p>
-        </figcaption>
-      </figure>
-    )}
-  </>
-}
+  const getKeywords = (description: string) => {
+    return description
+      ?.toLowerCase()
+      .split(" ")
+      .filter(word => word.length > 3 && !word.match(/very|with|through/)) // filter to ignore
+      .slice(0, 5); // 5 tags max
+  };
+
+  // Rendering the fetched images and displaying their descriptions and authors
+  return (
+    <div>
+      <h1>Gallery Images</h1>
+      <div className="gallery">
+        {images.map((image) => (
+          <figure key={image.id}>
+            <img src={image.urls.thumb} alt={image.alt_description} onClick={() => setSelectedImg(image)} />
+            <figcaption>
+              <h2>{image.alt_description}</h2>
+              <p>{image.user.name}</p>
+            </figcaption>
+          </figure>
+        ))}
+      </div>
+
+      {/* Modal */}
+      {selectedImg && (
+        <div className="modal">
+          <div className="modal-content">
+            <span className="close" onClick={() => setSelectedImg(null)}>
+              &times;
+            </span>
+            <img src={selectedImg.urls.regular} alt={selectedImg.alt_description} />
+            <h2>{selectedImg.alt_description}</h2>
+            <h3>Autor: {selectedImg.user.name}</h3>
+            <p>Tags: {getKeywords(selectedImg.alt_description).join(", ")}</p>
+          </div>
+        </div>
+      )}
+    </div>
+  );
+};
 
 export default UnsplashGallery
